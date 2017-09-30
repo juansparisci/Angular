0a4758550e0435244e8fc2ff5b0ed5f6dda4a78b
Juan <juan_sparisci@hotmail.com>
2017-09-30 17:57:40 -0300

    Tarea Práctica Nro 1

--- 2. SPA/src/app/components/heroe/heroe.component.ts
+++ 2. SPA/src/app/components/heroe/heroe.component.ts
@@ -1,23 +1,27 @@
 import { Component, OnInit } from '@angular/core';
-import {ActivatedRoute} from '@angular/router';
-import {HeroesService} from '../../servicios/heroes.service';
+import {ActivatedRoute,Router} from '@angular/router';
+import {HeroesService,Heroe} from '../../servicios/heroes.service';
+
 @Component({
   selector: 'app-heroe',
   templateUrl: './heroe.component.html'
 })
 export class HeroeComponent {
 
-  heroe:any={};
+  heroe:Heroe;
 
   constructor(private activatedRoute:ActivatedRoute,
-              private heroesService:HeroesService)
+              private heroesService:HeroesService,
+              private router:Router)
   {
-
-    this.activatedRoute.params.subscribe(params=>{
-      //  console.log( params["id"] )
+      this.activatedRoute.params.subscribe(params=>{
       this.heroe=heroesService.getHeroe(params["id"]);
      });
 
+   }
+   regresar()
+   {
+     this.router.navigate(['/heroes']);
    }
 
 }
