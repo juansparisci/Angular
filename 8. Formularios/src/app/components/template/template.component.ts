b2c7f3b226e6245f2c33cf8a19c844a9eb869f7d
Juan <juan_sparisci@hotmail.com>
2017-12-04 22:51:04 -0300

    Validación de formularios en template (html) usando ngModel. Clase 155

---
+++ 8. Formularios/src/app/components/template/template.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { NgForm} from '@angular/forms';
+
+@Component({
+  selector: 'app-template',
+  templateUrl: './template.component.html',
+  styles: [`
+          .ng-invalid.ng-touched:not(form){
+            border:1px solid red;
+          }
+    `]
+})
+export class TemplateComponent {
+  usuario:Object={
+    nombre:null,
+    apellido:null,
+    correo:null
+  }
+  constructor() { }
+
+  guardar(forma:NgForm) {
+    console.log("ngForm: ", forma);
+    console.log("Valor Forma: ",forma.value);
+    console.log("Usuario: ",this.usuario);
+  }
+
+}
