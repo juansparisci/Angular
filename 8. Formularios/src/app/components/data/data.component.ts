c1318063337bacafd074ebb07271f1e144297cb8
Juan <juan_sparisci@hotmail.com>
2017-12-06 00:02:09 -0300

    Formularios con lógica el typescript. Validaciones, accesos a datos, etc. Clase 161

--- 8. Formularios/src/app/components/data/data.component.ts
+++ 8. Formularios/src/app/components/data/data.component.ts
@@ -1,15 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import {FormGroup,FormControl,Validators} from '@angular/forms';
+
 
 @Component({
   selector: 'app-data',
   templateUrl: './data.component.html',
   styles: []
 })
-export class DataComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+export class DataComponent {
+  usuario:Object={
+    nombreCompleto:{
+      nombre : "Juan",
+      apellido : "Sparisci"
+    },
+    correo:"juan@juan.com"
   }
+
+  forma:FormGroup;
+  constructor() {
+    console.log(this.usuario);
+this.forma=new FormGroup({
+  'nombreCompleto':new FormGroup({
+    'nombre':new FormControl('', [Validators.required,
+                                  Validators.minLength(3)]),
+    'apellido':new FormControl('', Validators.required),
+  }),
+  'correo':new FormControl('',
+                            [
+                              Validators.required,
+                              Validators.pattern("[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$")
+                            ])
+});
+   }
+   guardarCambios()
+   {
+      console.log(this.forma.value);
+      console.log(this.forma);
+   }
+
 
 }
