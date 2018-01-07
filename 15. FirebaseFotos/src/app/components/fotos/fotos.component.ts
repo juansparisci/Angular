d3d20e783dc67c521fb643e3edae768dc2da0459
Juan <juan_sparisci@hotmail.com>
2018-01-06 22:29:25 -0300

    Carga a firebase. Clase 251

--- 15. FirebaseFotos/src/app/components/fotos/fotos.component.ts
+++ 15. FirebaseFotos/src/app/components/fotos/fotos.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { CargaImagenesService  } from '../../services/carga-imagenes.service';
+import { FirebaseListObservable } from 'angularfire2/database-deprecated';
 
 @Component({
   selector: 'app-fotos',
   templateUrl: './fotos.component.html',
   styles: []
 })
-export class FotosComponent implements OnInit {
+export class FotosComponent {
+  imagenes: FirebaseListObservable<any[]>;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private _cargarImagenes: CargaImagenesService) {
+    this.imagenes = this._cargarImagenes.listaUltimasImagenes( 10 );
   }
 
 }
--- 15. FirebaseFotos/src/app/services/carga-imagenes.service.ts
+++ 15. FirebaseFotos/src/app/services/carga-imagenes.service.ts
@@ -16,6 +16,26 @@ export class CargaImagenesService {
   }
   cargarImagenesHaciaFirebase(archivos: FileItem[]) {
     console.log(archivos);
+    const storageRef = firebase.storage().ref();
+    for ( const item of archivos ){
+      item.estaSubiendo = true;
+      const uploadTask: firebase.storage.UploadTask =
+            storageRef.child(`${this.CARPETA_IMAGENES}/${item.NombreArchivo}`)
+            .put(item.Archivo);
+      uploadTask.on(firebase.storage.TaskEvent.STATE_CHANGED,
+                    (snapshot) => item.progreso = ( uploadTask.snapshot.bytesTransferred /  uploadTask.snapshot.totalBytes) * 100,
+                    ( error ) => console.error('Error al subir', error),
+                    () => {
+                      item.url = uploadTask.snapshot.downloadURL;
+                      item.estaSubiendo = false;
+                      this.guardarImagen({
+                        nombre: item.NombreArchivo,
+                        url: item.url
+                      });
+                    }
+                    
+      )
+    }
   }
   private guardarImagen(imagen: any) {
     this.af.list(`${this.CARPETA_IMAGENES}`).push(imagen);
